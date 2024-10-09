39018ec17d13b9436d174137bd264f4bc2aa675a
Hasan <hasanazeerkhan@gmail.com>
2024-10-09 11:55:04 +0530

    feat: added test case

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,21 @@
+describe('Login page tests', function(){
+    
+    it('Website title', function(){
+        
+    })
+    it('Login - correct credentials', function(){
+        
+    })
+    it('Login - incorrect credentials', function(){
+        
+    })
+    it('Forgot password functionality', function(){
+        
+    })
+    it('Social handles links', function(){
+
+    })
+    it('Other website links', function(){
+
+    })
+})
