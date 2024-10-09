e05417276281e17ce3ed01c1d8e5ab4de21e290f
Hasan <hasanazeerkhan@gmail.com>
2024-10-09 15:32:20 +0530

    feat: left pane related test cases

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -30,40 +30,125 @@ const credentials = {
     accountName: 'Atharva  More'
 }
 
-describe('Login page tests', function(){
-    beforeEach(function(){
+const leftPane = {
+    element:'[class="oxd-text oxd-text--span oxd-main-menu-item--name"]',
+    name:{
+    admin: 'Admin',
+    pim:'PIM',
+    leave:'Leave',
+    time:'Time',
+    recruitment:'Recruitment',
+    info: 'My Info',
+    performance:'Performance',
+    dashboard:'Dashboard',
+    directory: 'Directory',
+    maintenance:'Maintenance',
+    claim: 'Claim',
+    buzz:'Buzz',
+    },
+    url: {
+        admin: 'https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewSystemUsers',
+        pim:'https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewEmployeeList',
+        leave:'https://opensource-demo.orangehrmlive.com/web/index.php/leave/viewLeaveList',
+        time:'https://opensource-demo.orangehrmlive.com/web/index.php/time/viewEmployeeTimesheet',
+        recruitment:'https://opensource-demo.orangehrmlive.com/web/index.php/recruitment/viewCandidates',
+        info: 'https://opensource-demo.orangehrmlive.com/web/index.php/pim/viewPersonalDetails/empNumber/7',
+        performance:'https://opensource-demo.orangehrmlive.com/web/index.php/performance/searchEvaluatePerformanceReview',
+        dashboard:'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index',
+        directory: 'https://opensource-demo.orangehrmlive.com/web/index.php/directory/viewDirectory',
+        maintenance:'https://opensource-demo.orangehrmlive.com/web/index.php/maintenance/purgeEmployee',
+        claim: 'https://opensource-demo.orangehrmlive.com/web/index.php/claim/viewAssignClaim',
+        buzz:'https://opensource-demo.orangehrmlive.com/web/index.php/buzz/viewBuzz'
+    }
+
+}
+
+const maintenance = {
+    cancel: '[class="oxd-button oxd-button--large oxd-button--ghost orangehrm-admin-access-button"]'
+}
+
+// describe('Login page tests', function(){
+//     beforeEach(function(){
+//         cy.visit('/')
+//     })
+//     it('Website title', function(){
+//         cy.title().should('eq', login.pageTitle);
+//     })
+//     it('Login - correct credentials', function(){
+//         cy.get(login.userName).type(credentials.userName);
+//         cy.get(login.password).type(credentials.password);
+//         cy.get(login.loginButton).click();
+//         cy.url().should('eq', login.landingUrl);
+//         cy.get(login.accountName).should('have.text', credentials.accountName);
+        
+//     })
+//     it('Login - incorrect credentials', function(){
+//         cy.get(login.userName).type(credentials.userName);
+//         cy.get(login.password).type("bad password");
+//         cy.get(login.loginButton).click();
+//         cy.get(login.errorIcon).should('exist');
+//         cy.get(login.errorMessageElement).should('have.text',login.errorMessage);
+//         cy.url().should('eq',login.pageUrl);
+//     })
+//     it('Forgot password', function(){
+//         cy.get(login.forgotPassword).click();
+//         cy.url().should('eq', forgotPassword.url);
+//         cy.get(forgotPassword.cancel).click();
+//         cy.url().should('eq', login.pageUrl);
+//         cy.get(login.forgotPassword).click();
+//         cy.get(forgotPassword.resetPassword).click();
+//         cy.get(forgotPassword.error).should('have.text', forgotPassword.errorMessage);
+//         cy.get(forgotPassword.userName).type(credentials.userName);
+//         cy.get(forgotPassword.resetPassword).click();
+//         cy.get(forgotPassword.success).should('have.text', forgotPassword.successMessage);
+//         cy.url().should('eq', forgotPassword.urlAfterReset);
+//     })
+// })
+
+describe('Landing page tests', function(){
+        beforeEach(function(){
         cy.visit('/')
-    })
-    it('Website title', function(){
-        cy.title().should('eq', login.pageTitle);
-    })
-    it('Login - correct credentials', function(){
         cy.get(login.userName).type(credentials.userName);
         cy.get(login.password).type(credentials.password);
         cy.get(login.loginButton).click();
-        cy.url().should('eq', login.landingUrl);
-        cy.get(login.accountName).should('have.text', credentials.accountName);
+    })
+    it('URL of all the left pane actions', function(){
+        cy.get(leftPane.element).contains(leftPane.name.admin).click();
+        cy.url().should('eq', leftPane.url.admin);
         
-    })
-    it('Login - incorrect credentials', function(){
-        cy.get(login.userName).type(credentials.userName);
-        cy.get(login.password).type("bad password");
-        cy.get(login.loginButton).click();
-        cy.get(login.errorIcon).should('exist');
-        cy.get(login.errorMessageElement).should('have.text',login.errorMessage);
-        cy.url().should('eq',login.pageUrl);
-    })
-    it('Forgot password', function(){
-        cy.get(login.forgotPassword).click();
-        cy.url().should('eq', forgotPassword.url);
-        cy.get(forgotPassword.cancel).click();
-        cy.url().should('eq', login.pageUrl);
-        cy.get(login.forgotPassword).click();
-        cy.get(forgotPassword.resetPassword).click();
-        cy.get(forgotPassword.error).should('have.text', forgotPassword.errorMessage);
-        cy.get(forgotPassword.userName).type(credentials.userName);
-        cy.get(forgotPassword.resetPassword).click();
-        cy.get(forgotPassword.success).should('have.text', forgotPassword.successMessage);
-        cy.url().should('eq', forgotPassword.urlAfterReset);
+        cy.get(leftPane.element).contains(leftPane.name.pim).click();
+        cy.url().should('eq', leftPane.url.pim);
+
+        cy.get(leftPane.element).contains(leftPane.name.leave).click();
+        cy.url().should('eq', leftPane.url.leave);
+
+        cy.get(leftPane.element).contains(leftPane.name.time).click();
+        cy.url().should('eq', leftPane.url.time);
+
+        cy.get(leftPane.element).contains(leftPane.name.recruitment).click();
+        cy.url().should('eq', leftPane.url.recruitment);
+
+        cy.get(leftPane.element).contains(leftPane.name.info).click();
+        cy.url().should('eq', leftPane.url.info);
+
+        cy.get(leftPane.element).contains(leftPane.name.performance).click();
+        cy.url().should('eq', leftPane.url.performance);
+
+        cy.get(leftPane.element).contains(leftPane.name.dashboard).click();
+        cy.url().should('eq', leftPane.url.dashboard);
+
+        cy.get(leftPane.element).contains(leftPane.name.directory).click();
+        cy.url().should('eq', leftPane.url.directory);
+
+        cy.get(leftPane.element).contains(leftPane.name.maintenance).click();
+        cy.url().should('eq', leftPane.url.maintenance);
+        cy.get(maintenance.cancel).click();
+
+        cy.get(leftPane.element).contains(leftPane.name.claim).click();
+        cy.url().should('eq', leftPane.url.claim);
+
+        cy.get(leftPane.element).contains(leftPane.name.buzz).click();
+        cy.url().should('eq', leftPane.url.buzz);
+
     })
 })
