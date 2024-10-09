f7b094745f6f06d2eb1bb2781f915f233950ffd0
Hasan <hasanazeerkhan@gmail.com>
2024-10-09 12:50:20 +0530

    feat: test cases

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,21 +1,51 @@
+const login = {
+    userName:'[name="username"]',
+    password:'[name="password"]',
+    loginButton: '[type="submit"]',
+    pageTitle: 'OrangeHRM',
+    url: 'https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index',
+    accountName: '[class="oxd-userdropdown-name"]',
+    errorIcon: '[class="oxd-icon bi-exclamation-circle oxd-alert-content-icon"]',
+    errorMessageElement: '[class="oxd-text oxd-text--p oxd-alert-content-text"]',
+    errorMessage:'Invalid credentials'
+}
+
+const credentials = {
+    userName:'Admin',
+    password: 'admin123',
+    accountName: 'Sushil Patil'
+}
+
 describe('Login page tests', function(){
-    
+    beforeEach(function(){
+        cy.visit('/')
+    })
     it('Website title', function(){
-        
+        cy.title().should('eq', login.pageTitle);
     })
     it('Login - correct credentials', function(){
+        cy.get(login.userName).type(credentials.userName);
+        cy.get(login.password).type(credentials.password);
+        cy.get(login.loginButton).click();
+        cy.url().should('eq', login.url);
+        cy.get(login.accountName).should('have.text', credentials.accountName);
         
     })
     it('Login - incorrect credentials', function(){
+        cy.get(login.userName).type(credentials.userName);
+        cy.get(login.password).type("bad password");
+        cy.get(login.loginButton).click();
+        cy.get(login.errorIcon).should('exist');
+        cy.get(login.errorMessageElement).should('have.text',login.errorMessage);
+        cy.url().should('eq',"https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    })
+    // it('Forgot password functionality', function(){
         
-    })
-    it('Forgot password functionality', function(){
-        
-    })
-    it('Social handles links', function(){
+    // })
+    // it('Social handles links', function(){
 
-    })
-    it('Other website links', function(){
+    // })
+    // it('Other website links', function(){
 
-    })
+    // })
 })
